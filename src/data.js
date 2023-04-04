2c600a9535d5893bd649ff7ee3f47fdc162a9cb2
AmiRobinsonMuto <amimuto1002@gmail.com>
2023-04-04 09:13:15 -0700

    Add placeholder item data

---
+++ src/data.js
@@ -0,0 +1,111 @@
+import item from "./assets/item.png";
+
+export const data = [
+  {
+    id: 1,
+    name: "Lavender Essential Oil",
+    description:
+      "Relaxing and calming, lavender essential oil can help soothe the mind and promote restful sleep.",
+    price: 24.95,
+    image: item,
+  },
+  {
+    id: 2,
+    name: "Peppermint Essential Oil",
+    description:
+      "Cool and refreshing, peppermint essential oil can help soothe sore muscles and promote mental clarity.",
+    price: 18.95,
+    image: item,
+    category: "Wellness",
+  },
+  {
+    id: 3,
+    name: "Eucalyptus Essential Oil",
+    description:
+      "Clear and refreshing, eucalyptus essential oil can help promote clear breathing and support the immune system.",
+    price: 21.95,
+    image: item,
+    category: "Wellness",
+  },
+  {
+    id: 4,
+    name: "Tea Tree Essential Oil",
+    description:
+      "Cleansing and purifying, tea tree essential oil can help support healthy skin and reduce the appearance of blemishes.",
+    price: 16.95,
+    image: item,
+    category: "Skin Care",
+  },
+  {
+    id: 5,
+    name: "Lemon Essential Oil",
+    description:
+      "Uplifting and energizing, lemon essential oil can help promote a positive mood and support healthy digestion.",
+    price: 14.95,
+    image: item,
+    category: "Wellness",
+  },
+  {
+    id: 6,
+    name: "Rosemary Essential Oil",
+    description:
+      "Stimulating and invigorating, rosemary essential oil can help promote mental clarity and support healthy hair and scalp.",
+    price: 19.95,
+    image: item,
+    category: "Hair Care",
+  },
+  {
+    id: 7,
+    name: "Bergamot Essential Oil",
+    description:
+      "Refreshing and uplifting, bergamot essential oil can help promote a positive mood and support healthy skin.",
+    price: 26.95,
+    image: item,
+    category: "Hair Care",
+  },
+  {
+    id: 8,
+    name: "Frankincense Essential Oil",
+    description:
+      "Grounding and soothing, frankincense essential oil can help promote feelings of relaxation and spiritual awareness.",
+    price: 29.95,
+    image: item,
+    category: "Spiritual",
+  },
+  {
+    id: 9,
+    name: "Cedarwood Essential Oil",
+    description:
+      "Warm and woodsy, cedarwood essential oil can help promote relaxation and support healthy respiratory function.",
+    price: 17.95,
+    image: item,
+    category: "Relaxation",
+  },
+  {
+    id: 10,
+    name: "Lemongrass Essential Oil",
+    description:
+      "Invigorating and energizing, lemongrass essential oil can help promote mental clarity and support healthy digestion.",
+    price: 12.95,
+    image: item,
+    category: "Wellness",
+  },
+  {
+    id: 11,
+    name: "Geranium Essential Oil",
+    description:
+      "Balancing and harmonizing, geranium essential oil can help promote healthy skin and support emotional balance.",
+    price: 23.95,
+    image: item,
+    category: "Skin Care",
+  },
+  {
+    id: 12,
+    name: "Ylang Ylang Essential Oil",
+    description:
+      "Sensual and floral, ylang ylang essential oil can help promote relaxation and support healthy hair and skin.",
+    price: 28.95,
+    image: item,
+    category: "Beauty",
+  },
+];

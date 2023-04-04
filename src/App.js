74993fb21cec9b40448ead2a2a8c2d405cf08427
AmiRobinsonMuto <amimuto1002@gmail.com>
2023-04-04 09:54:25 -0700

    Add placeholder footer and basic styling

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Cart from "./pages/Cart/Cart";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import { data } from "./data";
+import Footer from "./components/Footer";
 
 function App() {
   const [items, setItems] = useState(data);
@@ -19,6 +20,7 @@ function App() {
           <Route path="/shop" element={<Shop items={items} />} />
           <Route path="/cart" element={<Cart />} />
         </Routes>
+        <Footer />
       </Router>
     </>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div class="footer-container">
+        <div class="footer-row">
+          <div class="footer-col">
+            <h4>About Us</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quis
+              dui vel metus sagittis facilisis. Nam ultrices, sapien non blandit
+              gravida, lorem enim blandit arcu, eget hendrerit urna arcu at
+              mauris.
+            </p>
+          </div>
+          <div class="footer-col">
+            <h4>Customer Service</h4>
+            <ul>
+              <li>
+                <a href="#">Contact Us</a>
+              </li>
+              <li>
+                <a href="#">FAQs</a>
+              </li>
+              <li>
+                <a href="#">Shipping</a>
+              </li>
+              <li>
+                <a href="#">Returns</a>
+              </li>
+            </ul>
+          </div>
+          <div class="footer-col">
+            <h4>My Account</h4>
+            <ul>
+              <li>
+                <a href="#">My Orders</a>
+              </li>
+              <li>
+                <a href="#">My Account</a>
+              </li>
+              <li>
+                <a href="#">Wishlist</a>
+              </li>
+              <li>
+                <a href="#">Newsletter</a>
+              </li>
+            </ul>
+          </div>
+          <div class="footer-col">
+            <h4>Follow Us</h4>
+            <ul class="social-icons">
+              <li>
+                <a href="#">
+                  <FaFacebook />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <FaTwitter />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <FaInstagram />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;

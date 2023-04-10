43f0ede445ff57b07fe2f294aecfd135e576b142
AmiRobinsonMuto <amimuto1002@gmail.com>
2023-04-10 10:55:56 -0700

    Write tests for DeleteButton.js

---
+++ src/pages/Cart/DeleteButton.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import DeleteButton from "./DeleteButton";
+import "@testing-library/jest-dom";
+
+describe("DeleteButton", () => {
+  test("renders a delete button", () => {
+    render(<DeleteButton itemId={1} removeItem={() => {}} />);
+    const deleteButton = screen.getByTestId("delete-button");
+    expect(deleteButton).toBeInTheDocument();
+    expect(deleteButton).toHaveClass("button", "delete-button");
+  });
+
+  test("calls the removeItem callback when clicked", async () => {
+    const removeItemMock = jest.fn();
+    const user = userEvent.setup();
+    render(<DeleteButton itemId={1} removeItem={removeItemMock} />);
+    const deleteButton = screen.getByTestId("delete-button");
+    await user.click(deleteButton);
+    expect(removeItemMock).toHaveBeenCalledTimes(1);
+  });
+});
